ded6dee6b8fde5472a8bf2e8a167ac0e8b980c0a
Vinicius Escano <vini.e@outlook.com>
2024-08-24 19:01:14 -0300

    TASK-222 - Criação de ambiente

---
+++ src/main/java/com/platcon/platcon/PlatconApplication.java
@@ -0,0 +1,13 @@
+package com.platcon.platcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platcon/platcon/PlatconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platcon.platcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
